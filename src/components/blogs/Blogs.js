9b051b54288a34fa45c6277c27901e891461df37
tushart681 <tushart681@gmail.com>
2022-10-12 23:19:40 +0600

    7th commit

--- src/components/blogs/Blogs.js
+++ src/components/blogs/Blogs.js
@@ -13,7 +13,7 @@ const Blogs = () => {
             <br />
             <h1>What is useRef?</h1>
             <br />
-            <p>The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.</p>
+            <p>Ans: The useRef Hook allows you to persist values between renders. It can be used to store a mutable value that does not cause a re-render when updated. It can be used to access a DOM element directly.</p>
 
         </div>
     );
